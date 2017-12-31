e8b4201cd752f302c10025268b9f8e94629b5512
Joshua Loyal <jloyal25@gmail.com>
2017-12-30 21:16:24 -0500

    added svc with tree kernel example

---
+++ examples/digits_svc.py
@@ -0,0 +1,30 @@
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.pipeline import make_pipeline
+from sklearn.svm import SVC
+
+from forest_kernels import ExtraTreesClassifierKernel
+
+
+digits = load_digits()
+X_train, X_test, y_train, y_test = train_test_split(
+    digits.data, digits.target, test_size=0.2, random_state=123)
+
+
+rf_kernel_svc = make_pipeline(
+    ExtraTreesClassifierKernel(
+        n_estimators=500,
+        kernel_type='leaves',
+        sampling_method='bootstrap',
+        random_state=123),
+    SVC(kernel='precomputed')
+)
+
+rf_kernel_svc.fit(X_train, y_train)
+y_pred = rf_kernel_svc.predict(X_test)
+print(accuracy_score(y_test, y_pred))
+
+svc = SVC(gamma=0.001).fit(X_train, y_train)
+y_pred = svc.predict(X_test)
+print(accuracy_score(y_test, y_pred))
